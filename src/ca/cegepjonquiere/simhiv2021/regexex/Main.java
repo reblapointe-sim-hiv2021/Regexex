d3680e1819a48960c864c586f93c966197873ef4
RebeccaLapointe <reblapointe@gmail.com>
2021-01-26 09:42:59 -0500

    loop through matches

--- src/ca/cegepjonquiere/simhiv2021/regexex/Main.java
+++ src/ca/cegepjonquiere/simhiv2021/regexex/Main.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        // CODE POSTAL ********************************************************
         Pattern patronCodePostal = Pattern.compile("([a-zA-Z][0-9]){3}");
 
         Scanner in = new Scanner(System.in);
@@ -13,16 +14,28 @@ public class Main {
         System.out.println("Entrez un code postal");
         String entree = in.nextLine();
 
-        boolean matche = patronCodePostal.matcher(entree).find();
+        Matcher matche = patronCodePostal.matcher(entree);
 
-        if(matche)
-            System.out.println("Merci!");
+        if(matche.find())
+            System.out.println("Merci! Code postal entré : " + matche.group());
         else
             System.out.println("Ceci n'est pas un code postal");
 
+
+
+        // NOMBRES ************************************************************
+        Pattern nombre = Pattern.compile("[\\d]+(,[\\d]+)?");
+        System.out.println("Entrez des nombres, avec ou sans virgules");
+        entree = in.nextLine();
+
+        matche = nombre.matcher(entree);
+
+        System.out.println("Voici tous les nombres que vous avez écrit : ");
+        while (matche.find())
+            System.out.println(matche.group());
+
+
+        // CODE COURS *********************************************************
         Pattern codeCoursInfo = Pattern.compile("420-[A-Z]{3}-JQ");
-
-        Pattern nombre = Pattern.compile("^[0-9]+(,[0-9]+)?$");
-
     }
 }
